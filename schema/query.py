4bd3d044a81931525ba96d9dd5f9c8fd272c2747
Kostya Morozov <kostet499@gmail.com>
2019-11-15 23:13:20 +0200

    Add create and get Vote

--- schema/query.py
+++ schema/query.py
@@ -12,7 +12,7 @@ def create_user(id, user_name=''):
 
 
 def create_poll(owner_id, type_title, config, title='', desc=''):
-    poll_bool = models.Poll.get_or_create(owner_id=models.User.get_by_id(owner_id), type=type_title,
+    poll_bool = models.Poll.get_or_create(owner=models.User.get_by_id(owner_id), type=type_title,
                                           config_json=config, title=title, description=desc)
     id = poll_bool[0].id
     if poll_bool[1]:
@@ -25,3 +25,23 @@ def create_poll(owner_id, type_title, config, title='', desc=''):
 def get_poll_data(poll_id):
     poll = models.Poll.get_by_id(poll_id)
     return poll.owner_id, poll.type, poll.config_json
+
+
+def create_vote(user_id, poll_id, answer_json):
+    user = models.User.get_by_id(user_id)
+    poll = models.Poll.get_by_id(poll_id)
+    try:
+        vote_bool = models.Votes.get_or_create(user=user, poll=poll, answer_json=answer_json)[1]
+    except models.IntegrityError:
+        vote_bool = False
+
+    if vote_bool:
+        print('Vote created with user_id, poll_id =', str(user_id) + ',', poll_id)
+    else:
+        print('Vote with user_id, poll_id =', str(user_id) + ',', poll_id, 'already exists')
+    return user_id, poll_id
+
+
+def get_vote_data(user_id, poll_id):
+    vote = models.Votes.get_by_id((user_id, poll_id))
+    return vote.user_id, vote.poll_id, vote.answer_json
