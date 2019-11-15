53cf3fb62c6df2e37b17e003b44c4d9db95d7f08
2ToThe10th <grib.a@phystech.edu>
2019-11-15 21:11:22 +0200

    print changed on bot send

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,7 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-import lib
+from lib import get_types, creation, get_creator_answers
+import schema.query as queries
 
 TOKEN = "783657766:AAHh0XwRqUoYseLKyxZxhPr-vwhukp9iMCc"
 
@@ -10,18 +11,18 @@ users_create_now = dict()
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    print("Hello. Make vote")
+    bot.send_message(message.chat.id, "Hello. Make vote")
     if message.chat.username is not None:
-        lib.queries.create_user(message.chat.id, message.chat.username)
+        queries.create_user(message.chat.id, message.chat.username)
     else:
-        lib.queries.create_user(message.chat.id)
+        queries.create_user(message.chat.id)
 
 
 @bot.message_handler(commands=['create'])
 def create_evote(message):
     users_create_now.pop(message.chat.id, None)
     users_create_now[message.chat.id] = None
-    vote_types = lib.get_types()
+    vote_types = get_types()
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
     for vote_type in vote_types:
@@ -33,7 +34,7 @@ def create_evote(message):
 def callback_evote(call):
     if call.message.chat.id in users_create_now and users_create_now[call.message.chat.id] is None:
         vote_type = call.data[4:]
-        questions = lib.creation(vote_type)
+        questions = creation(vote_type)
         users_create_now[call.message.chat.id] = {'type': vote_type, 'question': [i[1] for i in questions], 'question_header': [i[0] for i in questions], 'answer': []}
         bot.send_message(call.message.chat.id, questions[0][1])
         print(users_create_now)
@@ -45,7 +46,7 @@ def ask_and_create_evote(message):
     if len(users_create_now[message.chat.id]['answer']) == len(users_create_now[message.chat.id]['question']):
         to_create_answer = [[users_create_now[message.chat.id]['question_header'][i], users_create_now[message.chat.id]['answer'][i]] for i in range(len(users_create_now[message.chat.id]['question_header']))]
 
-        if lib.get_creator_answers(message.chat.id, users_create_now[message.chat.id]['type'], to_create_answer):
+        if get_creator_answers(message.chat.id, users_create_now[message.chat.id]['type'], to_create_answer):
             bot.send_message(message.chat.id, "Voting created")
         else:
             bot.send_message(message.chat.id, "ERROR")
