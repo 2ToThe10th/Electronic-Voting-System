c4b0ed737954e8b9de4ffeaf99111ae2131b6cc5
2ToThe10th <grib.a@phystech.edu>
2019-11-16 00:55:13 +0200

    buttons disappear

--- telegram_bot.py
+++ telegram_bot.py
@@ -22,26 +22,24 @@ def start(message):
 @bot.message_handler(commands=['create'])
 def create_create_evote(message):
     users_create_now.pop(message.chat.id, None)
-    users_create_now[message.chat.id] = None
     vote_types = get_types()
     markup = InlineKeyboardMarkup()
     markup.row_width = 1
     for vote_type in vote_types:
         markup.add(InlineKeyboardButton(vote_type, callback_data="type" + vote_type))
-    bot.send_message(message.chat.id, "Select type of electronic voting system:", reply_markup=markup)
+    users_create_now[message.chat.id] = {'type_message': bot.send_message(message.chat.id, "Select type of electronic voting system:", reply_markup=markup)}
 
 
-@bot.callback_query_handler(func=lambda call: len(call.data) >= 4 and call.data[:4] == "type")
+@bot.callback_query_handler(func=lambda call: len(call.data) >= 4 and call.data[:4] == "type" and call.message.chat.id in users_create_now)
 def callback_evote(call):
-    if call.message.chat.id in users_create_now and users_create_now[call.message.chat.id] is None:
-        vote_type = call.data[4:]
-        questions = creation(vote_type)
-        users_create_now[call.message.chat.id] = {'type': vote_type, 'question': [i[1] for i in questions], 'question_header': [i[0] for i in questions], 'answer': []}
-        bot.send_message(call.message.chat.id, questions[0][1])
-        print(users_create_now)
+    vote_type = call.data[4:]
+    bot.edit_message_text("Your choice: " + vote_type, chat_id=call.message.chat.id, message_id=users_create_now[call.message.chat.id]['type_message'].message_id)
+    questions = creation(vote_type)
+    users_create_now[call.message.chat.id] = {'type': vote_type, 'question': [i[1] for i in questions], 'question_header': [i[0] for i in questions], 'answer': []}
+    bot.send_message(call.message.chat.id, questions[0][1])
 
 
-@bot.message_handler(func=lambda message: (message.chat.id in users_create_now and users_create_now[message.chat.id] is not None and users_create_now[message.chat.id].get('answer') is not None))
+@bot.message_handler(func=lambda message: message.chat.id in users_create_now and users_create_now[message.chat.id] is not None and users_create_now[message.chat.id].get('answer') is not None)
 def ask_and_create_evote(message):
     users_create_now[message.chat.id]['answer'].append(message.text)
     if len(users_create_now[message.chat.id]['answer']) == len(users_create_now[message.chat.id]['question']):
@@ -61,15 +59,15 @@ def ask_and_create_evote(message):
 
 @bot.message_handler(commands=['vote'])
 def print_vote_with_code(message):
-    #try:
-    code = int(message.text[5:])
-    vote_type, (vote_question, vote_answers) = vote(code)
-    #except:
-    #    bot.send_message(message.chat.id, "after /vote might go code of vote")
-    #    return
+    try:
+        code = int(message.text[5:])
+        vote_type, (vote_question, vote_answers) = vote(code)
+    except:
+        bot.send_message(message.chat.id, "after /vote might go code of vote")
+        return
 
     users_vote_now.pop(message.chat.id, None)
-    users_vote_now[message.chat.id] = {'type': vote_type, 'code': code}
+    users_vote_now[message.chat.id] = {'type': vote_type, 'code': code, 'vote_question': vote_question, 'vote_answer': vote_answers}
 
     if vote_type == "choose_one":
         markup = InlineKeyboardMarkup()
@@ -84,7 +82,7 @@ def print_vote_with_code(message):
             send_message += '\n'
             markup.add(InlineKeyboardButton(index, callback_data="vote" + str(index)))
 
-        bot.send_message(message.chat.id, send_message, reply_markup=markup)
+        users_vote_now[message.chat.id]['message_sended'] = bot.send_message(message.chat.id, send_message, reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: len(call.data) >= 4 and call.data[:4] == "vote" and call.message.chat.id in users_vote_now and users_vote_now[call.message.chat.id] is not None)
@@ -93,7 +91,8 @@ def callback_vote_evote(call):
     if users_vote_now[call.message.chat.id]['type'] == "choose_one":
         vote_answer = int(call.data[4:])
         get_vote(call.message.chat.id, users_vote_now[call.message.chat.id]['code'], vote_answer)
-        bot.send_message(call.message.chat.id, "You vote is really important for us")
+        bot.edit_message_text(users_vote_now[call.message.chat.id]['vote_question'] + "\nYour choice: " + users_vote_now[call.message.chat.id]['vote_answer'][vote_answer - 1], chat_id=call.message.chat.id, message_id=users_vote_now[call.message.chat.id]['message_sended'].message_id)
+        bot.send_message(call.message.chat.id, "Your vote is really important for us")
         users_vote_now.pop(call.message.chat.id, None)
 
 
