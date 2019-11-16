f56239cc559f74793a90c33cb232b93ea196888a
korvin001 <geller.m@phystech.edu>
2019-11-16 11:57:37 +0200

    stats

--- statlib.py
+++ statlib.py
@@ -12,9 +12,35 @@ def stats_choose_one(data, config, id):
     plt.close()
 
 
+def stats_choose_many(data, config, id):
+    data2 =[]
+    for i in data:
+        print(i)
+        i['answer_json'] = list(map(int, json.loads(i['answer_json'])))
+        data2.extend(i['answer_json'])
+    n = len(config['variants'])
+    if len(data2):
+        plt.hist(data2, range=(1, n+1), bins=n, rwidth=0.2)
+        dd = {}
+        for i in data2:
+            if i not in dd:
+                dd[i] = 1
+            else:
+                dd[i] += 1
+
+        plt.xticks([0.5 + i for i in range(1, n + 1)], config['variants'])
+        print(dd)
+        plt.yticks(list(range(0, max(dd.values()) + 2)))
+        plt.savefig('hists/hist' + str(id))
+        plt.close()
+        return True
+    else:
+        return False
+
 
 custom_stats = {
     'choose_one': stats_choose_one,
+    'choose_many': stats_choose_many,
 }
 
 
