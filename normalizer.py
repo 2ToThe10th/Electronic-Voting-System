0951dcdae3871342b6e2b8568a94b46c953ca9d4
korvin001 <geller.m@phystech.edu>
2019-11-16 02:12:01 +0200

    prioritets

--- normalizer.py
+++ normalizer.py
@@ -5,6 +5,7 @@ def choose_one_normalizer(poll):
     except Exception:
         return False
 
+
 def choose_many_normalizer(poll):
     try:
         poll['variants'] = poll['variants'].strip().split(',')
@@ -13,7 +14,18 @@ def choose_many_normalizer(poll):
         return False
 
 
+def choose_prioritets_normalizer(poll):
+    try:
+        poll['variants'] = poll['variants'].strip().split(',')
+        poll['power'] = int(poll['power'])
+        return True
+    except Exception:
+        return False
+
+
+
 normalize_config = {
     'choose_one': choose_one_normalizer,
     'choose_many': choose_many_normalizer,
+    'choose_prioritets': choose_prioritets_normalizer,
 }
--- vote_functions.py
+++ vote_functions.py
@@ -1,11 +1,17 @@
 def choose_one_vote(poll):
     return poll['question'], poll['variants']
 
+
 def choose_many_vote(poll):
+    return poll['question'], poll['variants']
+
+
+def choose_many_prioritets(poll):
     return poll['question'], poll['variants']
 
 
 vote_data = {
     'choose_one': choose_one_vote,
     'choose_many': choose_many_vote,
+    'choose_prioritets': choose_many_prioritets
 }
