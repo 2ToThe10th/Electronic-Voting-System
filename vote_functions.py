e1d8551606c03248971851b2c0c84afbf5a9a4f5
korvin001 <geller.m@phystech.edu>
2019-11-16 02:20:40 +0200

    prioritets

--- vote_functions.py
+++ vote_functions.py
@@ -6,12 +6,12 @@ def choose_many_vote(poll):
     return poll['question'], poll['variants']
 
 
-def choose_many_prioritets(poll):
-    return poll['question'], poll['variants']
+def choose_prioritets(poll):
+    return poll['question'], poll['variants'], poll['power']
 
 
 vote_data = {
     'choose_one': choose_one_vote,
     'choose_many': choose_many_vote,
-    'choose_prioritets': choose_many_prioritets
+    'choose_prioritets': choose_prioritets
 }
